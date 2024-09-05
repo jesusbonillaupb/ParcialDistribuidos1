8c2eab91b73c7c41ccc1c2e476726ab4b9fa89a4
angieclemus <angielem11@gmail.com>
2024-09-04 23:20:17 -0500

    usermanager, servicioarchivos, rmi interfaz

--- RMIclient/src/NFS/interfaz/RMIArchivosInterfaz.java
+++ RMIclient/src/NFS/interfaz/RMIArchivosInterfaz.java
@@ -3,12 +3,9 @@ package NFS.interfaz;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.List;
 
 
 public interface RMIArchivosInterfaz extends Remote {
-   
-    //public int loginUser(String name,String password) throws RemoteException;  
-    
-    
+    String readFile(String username, String filePath) throws RemoteException;
+    void writeFile(String username, String filePath, String content) throws RemoteException;  
 }
